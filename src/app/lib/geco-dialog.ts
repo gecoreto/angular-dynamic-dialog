404383608d00a612c42a37002c8f79b2f0236199
David <elitgeco@hotmail.com>
2018-05-20 16:15:51 -0500

    ajustar demo

--- src/app/lib/geco-dialog.ts
+++
@@ -1,5 +0,0 @@
-export { GecoDialogModule } from './src/geco-dialog.module';
-export { ContainerDialogComponent } from './src/container-dialog.component';
-export { GECO_DATA_DIALOG, GECO_DIALOG_REF, GecoDialog } from './src/GecoDialog';
-export { DataConfig } from './src/data-config';
-export { GecoDialogRef } from './src/GecoDialogRef';
--- src/app/lib/src/data-config.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Configuration on opening a modal
- */
-export class DataConfig{
-    data?: {} | null;
-    useStyles?: string = 'default';
-}
